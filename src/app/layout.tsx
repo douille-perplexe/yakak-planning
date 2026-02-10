2f0d2ec71f38d08dd7373954423da8107b678e08
douille-perplexe <douille-perlexe@proton.me>
2026-02-10 23:00:40 +0100

    improve favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Yakak",
   description: "Plan outings with your friends",
+    icons: {
+        icon: "/icon.png",
+    },
 };
 
 export default function RootLayout({
